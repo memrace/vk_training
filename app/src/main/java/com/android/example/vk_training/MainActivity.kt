556dc732dd9a813f2cbc74dbb1bd74e13d74f2da
robbier-sozo <sozonovalexander13@gmail.com>
2020-07-06 18:08:55 +0300

    Network

--- app/src/main/java/com/android/example/vk_training/MainActivity.kt
+++ app/src/main/java/com/android/example/vk_training/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.android.example.vk_training
 
+
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import com.android.example.vk_training.databinding.ActivityMainBinding
 import com.google.android.material.textfield.TextInputEditText
-
+import com.android.example.vk_training.NetWorking.NetWork
+import java.io.IOException
+import java.net.URL
 
 
 class MainActivity : AppCompatActivity() {
@@ -27,5 +32,18 @@ class MainActivity : AppCompatActivity() {
 		resultTV = dB.resultTV
 
 // OnClickListener
+		findBT.setOnClickListener {
+			val generatedURL: URL = NetWork().generateURL(inputIdET.text.toString())
+			var response: String? = null
+			try {
+				response = NetWork().getResponseFromURL(generatedURL)
+			} catch (e:IOException) {
+				e.printStackTrace()
+			}
+
+			resultTV.text = response
+		}
+
+
 	}
 }
--- app/src/main/java/com/android/example/vk_training/NetWorking/NetWork.kt
+++ app/src/main/java/com/android/example/vk_training/NetWorking/NetWork.kt
@@ -1,4 +1,48 @@
 package com.android.example.vk_training.NetWorking
 
+import android.net.Uri
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
+import java.util.*
+import kotlin.time.measureTime
+
+// CONSTs
+
+private const val VK_URL: String = "https://api.vk.com"
+private const val VK_METHOD: String = "/method/users.get"
+private const val VK_V: String = "5.89"
+private const val PARAM_VERSION = "v"
+private const val PARAM_USER_ID = "user_ids"
+
+
 class NetWork {
+
+
+    fun generateURL(id:String):URL {
+        val builtURL: Uri = Uri.parse(VK_URL + VK_METHOD)
+            .buildUpon()
+            .appendQueryParameter(PARAM_USER_ID,id)
+            .appendQueryParameter(PARAM_VERSION,VK_V)
+            .build()
+
+        return URL(builtURL.toString())
+    }
+
+    fun getResponseFromURL(url:URL):String? {
+        val urlConnection: HttpURLConnection = url.openConnection() as HttpURLConnection
+         try {
+             val inputStream:InputStream = urlConnection.inputStream
+             val scanner = Scanner(inputStream)
+             scanner.useDelimiter("\\A")
+
+             return if (scanner.hasNext())
+                 scanner.next()
+             else null
+         } finally {
+             urlConnection.disconnect()
+         }
+
+
+    }
 }
